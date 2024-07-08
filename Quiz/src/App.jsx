9b22e1b2dd4d36bd050c7370740644f72a9a7ea2
alimombeni <ali.mombenni@gmail.com>
2024-07-08 18:08:09 +0330

    [ADD:Start component just bootstrap and jsx]

--- Quiz/src/App.jsx
+++ Quiz/src/App.jsx
@@ -1,14 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Quiz from "./components/Quiz.jsx";
 import Result from "./components/Result.jsx";
+import Start from "./components/Start.jsx";
 
 
 function App() {
 
   return (
       <>
-
-
+            <Start/>
               <Quiz/>
           <Result/>
 
---
+++ Quiz/src/components/Start.jsx
@@ -0,0 +1,23 @@
+
+
+
+
+const Start = () =>{
+
+    return (
+        <section className='bg-dark text-white text-center'>
+            <div className='container'>
+                <div className='row vh-100 align-items-center justify-content-center'>
+                    <div className='col-lg-8'>
+                        <h1 className='fw-bold mb-4 '>Basic react JS Quiz</h1>
+                            <button className='btn px-4 py-2 bg-light text-dark fw-bold'>Start Quiz</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+
+export default Start
